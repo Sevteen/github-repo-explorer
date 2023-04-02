9f293b93735d84ade7f336412a29186ee5c28411
ilhamhanif07 <ilhamhanif07@gmail.com>
2023-04-02 15:41:08 +0700

    fix: configuring setup with babel and webpack

---
+++ webpack.config.ts
@@ -0,0 +1,71 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+  entry: {
+    main: ['./src/index.tsx'],
+    vendor: ['react', 'react-dom'],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  optimization: {
+    usedExports: true,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    // new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: [path.resolve(__dirname, '.')],
+      },
+      {
+        test: /\.(png|jpeg|gif|svg)$/,
+        type: 'asset',
+        generator: {
+          filename: 'images/[name].[hash][ext][query]',
+        },
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    watchFiles: ['src/**/*'],
+    // hot: true,
+  },
+};
